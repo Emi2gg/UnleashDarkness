0c07807f535f5e5a076ab8074d7763ef14996c26
MigueQuinones <miguel.mtz.qns@gmail.com>
2024-03-17 23:01:19 -0600

    Code

---
+++ Assets/Scripts/Scene/Floor_Light.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor_Light : MonoBehaviour
+{
+    [SerializeField] private bool _isOn = false;
+    [SerializeField] MeshRenderer _floorBulb = null;
+    [SerializeField] private GameObject _floor = null;
+
+    [SerializeField] private Material _onMaterial, _offMaterial;
+
+    public void Start()
+    {
+        _isOn = false;
+        _floorBulb.material = _offMaterial;
+    }
+    public void Update()
+    {
+        if (_isOn)
+        {
+            _floorBulb.material = _onMaterial;
+            
+
+
+
+        }
+        
+        
+
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+
+        {
+            _isOn = true;
+        }
+    }
+
+}
